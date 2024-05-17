639e71a51d9fab2092fbdc5c37f2f644b139e010
Mary-No <mascha.novik@gmail.com>
2024-05-17 12:15:33 +0200

    lesson 75 completed

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import './style/index.css';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import store from './redux/redux-store'
-
 import {Provider} from "react-redux";
+
 
 
 ReactDOM.render(
